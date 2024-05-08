21b5ef2505074e870cafa5f5927692ffe73cfdc2
tecinfoifc <luansilva250807@gmail.com>
2024-05-08 18:36:33 -0300

    demanda

--- fabrica_needs/models/product.py
+++ fabrica_needs/models/product.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     amount = models.IntegerField(default=0, null=True, blank=True)
+    min = models.IntegerField(default=0, null=True, blank=True)
     appearence = models.ForeignKey(
         Image,
         related_name="+",
