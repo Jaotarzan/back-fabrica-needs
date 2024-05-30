9155937c171fd226fcadc5b5e5aa58df176e10f1
tecinfoifc <luansilva250807@gmail.com>
2024-05-30 16:26:09 -0300

    token configurado

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
