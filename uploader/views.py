911b23229f89d0887b22a4b886529bd7bec335cd
tecinfoifc <luansilva250807@gmail.com>
2024-06-10 21:42:27 -0300

    config de imagens

--- uploader/views.py
+++ uploader/views.py
@@ -1,20 +1,16 @@
-from rest_framework import mixins, parsers, viewsets
+from rest_framework import parsers, viewsets
 
 from uploader.models import Document, Image
 from uploader.serializers import DocumentUploadSerializer, ImageUploadSerializer
 
 
-class CreateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
-    pass
-
-
-class DocumentUploadViewSet(CreateViewSet):
+class DocumentUploadViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentUploadSerializer
     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
 
 
-class ImageUploadViewSet(CreateViewSet):
+class ImageUploadViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageUploadSerializer
     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
